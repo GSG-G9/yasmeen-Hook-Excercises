1ed2a3b449095ba5e1cd96a83d0cda28f9c0237d
yasmeenattallah <yattallah7@gmail.com>
2021-01-18 13:51:36 +0200

    exercise_1 solution

--- src/components/exercise_1.js
+++ src/components/exercise_1.js
@@ -1,9 +1,22 @@
+import React ,{ useState } from 'react';
+
 function ExerciseOne (){
-
-
+const [counter,setCounter]= useState(0);
+const handleAdd =()=>{
+setCounter(counter+1);
+}
+const handleSub =()=>{
+    setCounter(counter-1);
+    }
+    const handleRest =()=>{
+           setCounter(0);
+        }
 return (
     <div>
-        <p>test</p>
+     <p> Counter : {counter}</p>
+     <input type='button' value='Add' onClick={handleAdd}/>
+      <input type='button'  value='sub' onClick={handleSub}/>
+      <input type='button' value='reset' onClick={handleRest}/>
     </div>
 );
 
